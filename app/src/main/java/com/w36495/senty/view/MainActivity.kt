9d8fc82d746e58bc582d2cbef6fa879ae8baf2f1
w36495 <w36495@naver.com>
2024-05-06 16:38:50 +0900

    ui : Main 화면 작성

--- app/src/main/java/com/w36495/senty/view/MainActivity.kt
+++ app/src/main/java/com/w36495/senty/view/MainActivity.kt
@@ -2,25 +2,28 @@ package com.w36495.senty.view
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.w36495.senty.databinding.ActivityMainBinding
+import com.w36495.senty.view.screen.ui.theme.SentyTheme
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.w36495.senty.view.screen.MainScreen
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
-        // 로그인 액티비티로 이동
-        binding.mainEmailLogin.setOnClickListener {
-            val emailLoginIntent = Intent(this, SignInActivity::class.java)
-            startActivity(emailLoginIntent)
+        setContent {
+            SentyTheme {
+                MainScreen(
+                    onLoginClick = {
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    },
+                    onSignUpClick = {
+                        startActivity(Intent(this, SignupActivity::class.java))
+                    }
+                )
+            }
         }
-
-        // TODO 구글 이메일 로그인
     }
 }
