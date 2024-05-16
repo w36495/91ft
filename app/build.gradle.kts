69fb026a1550cd7408b6c16f83894371a1b21320
w36495 <w36495@naver.com>
2024-05-16 15:20:30 +0900

    depend : 의존성 추가 1) @Serializable 2) lifecycle-runtime-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,6 +111,7 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     implementation("androidx.compose.material3:material3")                       // UI
     implementation("androidx.compose.ui:ui")
@@ -156,7 +157,7 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
     // Kotlin Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     // Test
     testImplementation("junit:junit:4.13.2")
