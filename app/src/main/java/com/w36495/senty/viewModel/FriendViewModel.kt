47644a6560915ab26b176496ab583310f4f36119
w36495 <w36495@naver.com>
2024-05-19 18:42:57 +0900

    style : 불필요한 코드 제거

--- app/src/main/java/com/w36495/senty/viewModel/FriendViewModel.kt
+++ app/src/main/java/com/w36495/senty/viewModel/FriendViewModel.kt
@@ -1,18 +1,15 @@
 package com.w36495.senty.viewModel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.w36495.senty.domain.repository.FriendGroupRepository
 import com.w36495.senty.domain.repository.FriendRepository
 import com.w36495.senty.view.entity.FriendEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonObject
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,9 +17,6 @@ class FriendViewModel @Inject constructor(
     private val friendRepository: FriendRepository,
     private val friendGroupRepository: FriendGroupRepository
 ) : ViewModel() {
-    private val _friends = MutableStateFlow<List<FriendEntity>>(emptyList())
-    val friends = _friends.asStateFlow()
-
     val friends: StateFlow<List<FriendEntity>> = combine(
         friendRepository.getFriends(),
         friendGroupRepository.getFriendGroups()
@@ -41,28 +35,4 @@ class FriendViewModel @Inject constructor(
         started = SharingStarted.WhileSubscribed(5_000),
         initialValue = emptyList()
     )
-
-    fun saveFriend(friend: FriendEntity) {
-        viewModelScope.launch {
-            val result = friendRepository.insertFriend(friend.toDataEntity())
-
-            if (result.isSuccessful) {
-                result.body()?.let {
-                    val jsonObject = Json.decodeFromString<JsonObject>(it.string())
-                    val key = jsonObject["name"].toString().replace("\"", "")
-
-                    val friendKeyResult = friendRepository.patchFriendId(key)
-                    if (friendKeyResult.isSuccessful) {
-                        if (friendKeyResult.body()?.string() == it.string()) {
-                            // TODO : 등록 성공
-                        }
-                    } else {
-                        // TODO : 등록 실패
-                    }
-                }
-            } else {
-                Log.d("FriendViewModel", result.errorBody().toString())
-            }
-        }
-    }
 }
