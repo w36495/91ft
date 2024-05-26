69632962a4d5b94297792b1fd78cf679e807cbb9
w36495 <w36495@naver.com>
2024-05-26 18:20:40 +0900

    feat(friend-detail) : 선물 목록 가져오기 기능 구현

--- app/src/main/java/com/w36495/senty/viewModel/FriendDetailViewModel.kt
+++ app/src/main/java/com/w36495/senty/viewModel/FriendDetailViewModel.kt
@@ -5,13 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.w36495.senty.domain.repository.FriendGroupRepository
 import com.w36495.senty.domain.repository.FriendRepository
+import com.w36495.senty.domain.repository.GiftRepository
 import com.w36495.senty.view.entity.FriendDetail
+import com.w36495.senty.view.entity.gift.GiftDetailEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,24 +24,39 @@ import javax.inject.Inject
 class FriendDetailViewModel @Inject constructor(
     private val friendRepository: FriendRepository,
     private val friendGroupRepository: FriendGroupRepository,
+    private val giftRepository: GiftRepository,
 ) : ViewModel() {
     private var _snackMsg = MutableStateFlow("")
     val snackMsg: StateFlow<String> = _snackMsg.asStateFlow()
     private var _friend = MutableStateFlow(FriendDetail.emptyFriendEntity)
     val friend: StateFlow<FriendDetail> = _friend.asStateFlow()
+    private var _gifts = MutableStateFlow<List<GiftDetailEntity>>(emptyList())
+    val gifts: StateFlow<List<GiftDetailEntity>> = _gifts.asStateFlow()
 
     fun getFriend(friendId: String) {
         viewModelScope.launch {
-            friendGroupRepository.getFriendGroups().combine(
-                friendRepository.getFriend(friendId)
-            ) { groups, friend ->
-                val group = groups.find { it.id == friend.groupId }
+            friendRepository.getFriend(friendId).combine(
+                friendGroupRepository.getFriendGroups()
+            ) { friend, groups ->
+                val group = groups.find { it.id == friend.groupId }?.toDomainModel()
+
                 friend.toDomainEntity().apply {
-                    group?.toDomainModel()?.let { setFriendGroup(it) }
+                    setFriendGroup(group!!)
                 }
-            }.collect { friend ->
-                _friend.update { friend }
+            }.collectLatest {
+                _friend.value = it
             }
+        }
+    }
+
+    fun getGifts(friendId: String) {
+        viewModelScope.launch {
+            giftRepository.getGifts()
+                .map { gifts ->
+                    gifts.filter { it.friendId == friendId }.map { it.toDomainEntity() }
+                }.collectLatest {
+                    _gifts.value = it
+                }
         }
     }
 
