b602acc951668f38c83e8257d5bfbadbb753cf2f
w36495 <w36495@naver.com>
2024-06-04 17:17:43 +0900

    chore : 불필요한 파일 삭제

--- app/src/main/java/com/w36495/senty/view/listener/FriendSelectListener.kt
+++
@@ -1,9 +0,0 @@
-package com.w36495.senty.view.listener
-
-import com.w36495.senty.data.domain.Friend
-
-interface FriendSelectListener {
-
-    fun onFriendInfoClicked(friend: Friend)
-
-}
--- app/src/main/java/com/w36495/senty/view/listener/GiftSelectListener.kt
+++
@@ -1,9 +0,0 @@
-package com.w36495.senty.view.listener
-
-import com.w36495.senty.data.domain.Gift
-
-interface GiftSelectListener {
-
-    fun onGiftItemClicked(gift: Gift)
-
-}
