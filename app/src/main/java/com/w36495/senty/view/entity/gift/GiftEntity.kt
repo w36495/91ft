96a1a8c51ec3db8947089e47774103ac78841529
w36495 <w36495@naver.com>
2024-06-14 16:24:08 +0900

    chore : 불필요한 파일 삭제

--- app/src/main/java/com/w36495/senty/view/entity/gift/GiftEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.w36495.senty.view.entity.gift
-
-import com.w36495.senty.view.entity.FriendDetail
-
-data class GiftEntity(
-    val gift: GiftDetailEntity,
-    val friend: FriendDetail,
-    val giftImg: String = ""
-)
