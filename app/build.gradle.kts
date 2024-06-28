7dee593cf7fd6edc7556920ef000bb54da647992
w36495 <w36495@naver.com>
2024-06-28 17:23:35 +0900

    build : version upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.w36495.senty"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "3.6"
+        versionCode = 13
+        versionName = "3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
