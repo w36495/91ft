64da8eabefe37285f24997a15044aec295b7d9c5
w36495 <w36495@naver.com>
2025-04-18 14:33:39 +0900

    chore : 버전 카탈로그 toml 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")        // Navigation - Safe Args
-        classpath("com.google.gms:google-services:4.3.15")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
-
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.google.dagger.hilt.android) apply false
 }
