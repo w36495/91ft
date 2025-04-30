384ac97675de09476bce6117cf4e5dcf914cea07
w36495 <w36495@naver.com>
2025-04-30 15:15:52 +0900

    refactor : 비밀번호 재설정 발송 후, 토스트 표시

--- app/src/main/java/com/w36495/senty/view/screen/account/ResetPasswordDialogScreen.kt
+++ app/src/main/java/com/w36495/senty/view/screen/account/ResetPasswordDialogScreen.kt
@@ -1,5 +1,6 @@
 package com.w36495.senty.view.screen.account
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -20,6 +21,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -38,11 +40,16 @@ fun FindPasswordDialogScreen(
     vm: ResetPasswordViewModel = hiltViewModel(),
     onDismiss: () -> Unit,
 ) {
+    val context = LocalContext.current
+
     val hasEmailError by vm.hasEmailError.collectAsState()
     val emailErrorMsg by vm.emailErrorMsg.collectAsState()
     val result by vm.result.collectAsState()
 
-    if (result) onDismiss()
+    if (result) {
+        Toast.makeText(context, stringResource(id = R.string.reset_password_toast_text), Toast.LENGTH_SHORT).show()
+        onDismiss()
+    }
 
     var email by rememberSaveable { mutableStateOf("") }
 
