f607341b088b4ba9d8a0ede2d664c3f30a773d35
w36495 <w36495@naver.com>
2025-04-30 21:01:32 +0900

    refactor : 친구 그룹 수정 시, 다이얼로그 위 프로그레스바 표시

--- app/src/main/java/com/w36495/senty/view/screen/friendgroup/EditFriendGroupDialog.kt
+++ app/src/main/java/com/w36495/senty/view/screen/friendgroup/EditFriendGroupDialog.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,9 +30,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,13 +46,15 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.w36495.senty.R
 import com.w36495.senty.util.darken
-import com.w36495.senty.view.component.LoadingCircleIndicator
+import com.w36495.senty.util.dropShadow
 import com.w36495.senty.view.screen.friendgroup.model.EditFriendGroupContact
 import com.w36495.senty.view.screen.friendgroup.model.FriendGroupUiModel
 import com.w36495.senty.view.screen.ui.theme.SentyTheme
 import com.w36495.senty.view.ui.component.buttons.SentyFilledButton
 import com.w36495.senty.view.ui.component.dialogs.BasicColorPickerDialog
 import com.w36495.senty.view.ui.component.textFields.SentyTextField
+import com.w36495.senty.view.ui.theme.SentyBlack
+import com.w36495.senty.view.ui.theme.SentyWhite
 
 @Composable
 fun EditFriendGroupDialog(
@@ -59,24 +65,11 @@ fun EditFriendGroupDialog(
 ) {
     val uiState by vm.uiState.collectAsStateWithLifecycle()
 
-    var showProgressBar by remember { mutableStateOf(false) }
-    var showErrorSnackBar by remember { mutableStateOf<String?>(null) }
-
-    when (uiState) {
-        EditFriendGroupContact.State.Idle -> {}
-        EditFriendGroupContact.State.Loading -> {
-            showProgressBar = true
-        }
-        EditFriendGroupContact.State.Success ->{
-            showProgressBar = false
-        }
-    }
-
     LaunchedEffect(Unit) {
         vm.effect.collect { effect ->
             when (effect) {
                 is EditFriendGroupContact.Effect.ShowError -> {
-                    showErrorSnackBar = effect.message
+                    
                 }
                 is EditFriendGroupContact.Effect.NavigateFriendGroups -> {
                     onShowSnackBar(effect.message)
@@ -86,16 +79,8 @@ fun EditFriendGroupDialog(
         }
     }
 
-    when {
-        showProgressBar -> {
-            LoadingCircleIndicator()
-        }
-        showErrorSnackBar != null -> {
-            // TODO : 오류 메세지 처리 필요
-        }
-    }
-
     FriendGroupAddContents(
+        uiState = uiState,
         group = group,
         onDismiss = { onDismiss() },
         onClickSave = { vm.handleEvent(EditFriendGroupContact.Event.onSaveFriendGroup(it)) },
@@ -106,6 +91,7 @@ fun EditFriendGroupDialog(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun FriendGroupAddContents(
+    uiState: EditFriendGroupContact.State,
     group: FriendGroupUiModel?,
     onClickSave: (FriendGroupUiModel) -> Unit,
     onClickEdit: (FriendGroupUiModel) -> Unit,
@@ -117,6 +103,11 @@ private fun FriendGroupAddContents(
     var showColorPicker by remember { mutableStateOf(false) }
     var selectedColor by remember { mutableStateOf(group?.color ?: FriendGroupUiModel.DEFAULT_COLOR) }
     var isError by remember { mutableStateOf(false) }
+
+    var dialogHeight by remember { mutableStateOf(0) }
+    val dialogHeightDp = with(LocalDensity.current) {
+        dialogHeight.toDp()
+    }
 
     if (showColorPicker) {
         BasicColorPickerDialog(
@@ -137,88 +128,114 @@ private fun FriendGroupAddContents(
                     onClick = { focusManager.clearFocus() },
                 )
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.White)
-            ) {
-                CenterAlignedTopAppBar(
-                    title = {
-                        Text(
-                            text = stringResource(id = group?.let { R.string.friend_group_title_edit } ?: R.string.friend_group_title_save),
-                            style = SentyTheme.typography.headlineSmall
-                        )
-                    },
-                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.White),
-                    actions = {
-                        IconButton(onClick = { onDismiss() }) {
-                            Icon(imageVector = Icons.Default.Close, contentDescription = null)
-                        }
-                    }
-                )
-
-                Spacer(modifier = Modifier.height(12.dp))
-
-                Row(
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+                        .background(Color.White)
+                        .onSizeChanged { dialogHeight = it.height }
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .size(40.dp)
-                            .background(selectedColor, RoundedCornerShape(10.dp))
-                            .border(2.dp, selectedColor.darken(), RoundedCornerShape(10.dp))
-                            .clip(RoundedCornerShape(10.dp))
-                            .clickable { showColorPicker = true }
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Text(
+                                text = stringResource(id = group?.let { R.string.friend_group_title_edit } ?: R.string.friend_group_title_save),
+                                style = SentyTheme.typography.headlineSmall
+                            )
+                        },
+                        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.White),
+                        actions = {
+                            IconButton(onClick = { onDismiss() }) {
+                                Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                            }
+                        }
                     )
 
-                    Spacer(modifier = Modifier.width(4.dp))
+                    Spacer(modifier = Modifier.height(12.dp))
 
-                    SentyTextField(
-                        text = inputGroup,
-                        onChangeText = {
-                            inputGroup = when {
-                                it.length <= 8 -> {
-                                    isError = false
-                                    it
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(40.dp)
+                                .background(selectedColor, RoundedCornerShape(10.dp))
+                                .border(2.dp, selectedColor.darken(), RoundedCornerShape(10.dp))
+                                .clip(RoundedCornerShape(10.dp))
+                                .clickable { showColorPicker = true }
+                        )
+
+                        Spacer(modifier = Modifier.width(4.dp))
+
+                        SentyTextField(
+                            text = inputGroup,
+                            onChangeText = {
+                                inputGroup = when {
+                                    it.length <= 8 -> {
+                                        isError = false
+                                        it
+                                    }
+                                    else -> inputGroup.substring(0, 8)
                                 }
-                                else -> inputGroup.substring(0, 8)
+                            },
+                            hint = stringResource(id = R.string.friend_group_name_hint_text),
+                            hintSize = 14.sp,
+                            errorMsg = stringResource(id = R.string.friend_group_error),
+                            isError = isError,
+                            modifier = Modifier.padding(start = 2.dp),
+                            textStyle = SentyTheme.typography.bodyMedium,
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    SentyFilledButton(text = stringResource(id = group?.let { R.string.common_edit} ?: R.string.common_save),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 16.dp),
+                        onClick = {
+                            focusManager.clearFocus()
+
+                            if (inputGroup.isEmpty()) {
+                                isError = true
+                                return@SentyFilledButton
                             }
-                        },
-                        hint = stringResource(id = R.string.friend_group_name_hint_text),
-                        hintSize = 14.sp,
-                        errorMsg = stringResource(id = R.string.friend_group_error),
-                        isError = isError,
-                        modifier = Modifier.padding(start = 2.dp),
-                        textStyle = SentyTheme.typography.bodyMedium,
+
+                            group?.let {
+                                val editFriendGroup = it.copy(name = inputGroup, color = selectedColor)
+                                onClickEdit(editFriendGroup)
+                            } ?: run {
+                                val newFriendGroup = FriendGroupUiModel(name = inputGroup, color = selectedColor)
+                                onClickSave(newFriendGroup)
+                            }
+                        }
                     )
                 }
 
-                Spacer(modifier = Modifier.height(16.dp))
-
-                SentyFilledButton(text = stringResource(id = group?.let { R.string.common_edit} ?: R.string.common_save),
-                    modifier = Modifier
+                if (uiState is EditFriendGroupContact.State.Loading) {
+                    Box(modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 16.dp),
-                    onClick = {
-                        focusManager.clearFocus()
-
-                        if (inputGroup.isEmpty()) {
-                            isError = true
-                            return@SentyFilledButton
-                        }
-
-                        group?.let {
-                            val editFriendGroup = it.copy(name = inputGroup, color = selectedColor)
-                            onClickEdit(editFriendGroup)
-                        } ?: run {
-                            val newFriendGroup = FriendGroupUiModel(name = inputGroup, color = selectedColor)
-                            onClickSave(newFriendGroup)
+                        .height(dialogHeightDp)
+                        .background(SentyBlack.copy(0.2f), RoundedCornerShape(12.dp))) {
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .dropShadow(
+                                    shape = RoundedCornerShape(16.dp),
+                                    offsetX = 0.dp,
+                                    offsetY = 0.dp,
+                                    blur = 8.dp
+                                )
+                                .background(SentyWhite, RoundedCornerShape(16.dp))
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.padding(24.dp)
+                            )
                         }
                     }
-                )
+                }
             }
         }
     }
@@ -229,6 +246,7 @@ private fun FriendGroupAddContents(
 private fun FriendGroupAddDialogScreenPreview() {
     SentyTheme {
         FriendGroupAddContents(
+            uiState = EditFriendGroupContact.State.Loading,
             group = null,
             onClickEdit = {},
             onClickSave = {},
