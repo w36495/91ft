262d6d429f80319672640da3a3a1f24da1ecebb7
w36495 <w36495@naver.com>
2025-05-01 14:32:09 +0900

    feat : 홈 - 기념일 UiModel

---
+++ app/src/main/java/com/w36495/senty/view/screen/home/model/HomeScheduleUiModel.kt
@@ -0,0 +1,8 @@
+package com.w36495.senty.view.screen.home.model
+
+data class HomeScheduleUiModel(
+    val id: String = "",
+    val title: String = "",
+    val date: String = "",
+    val count: Int = 0,
+)
