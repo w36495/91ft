16d6fd9068168cbb037210939ad3a66637faeefb
w36495 <w36495@naver.com>
2025-05-02 15:24:35 +0900

    refactor : 선물 수정 시, 친구 정보 업데이트 UseCase로 분리

---
+++ app/src/main/java/com/w36495/senty/domain/usecase/UpdateGiftUseCase.kt
@@ -0,0 +1,37 @@
+package com.w36495.senty.domain.usecase
+
+import android.util.Log
+import com.w36495.senty.data.domain.GiftType
+import com.w36495.senty.domain.entity.Gift
+import com.w36495.senty.domain.repository.FriendRepository
+import com.w36495.senty.domain.repository.GiftRepository
+import javax.inject.Inject
+
+class UpdateGiftUseCase @Inject constructor(
+    private val giftRepository: GiftRepository,
+    private val friendRepository: FriendRepository,
+) {
+    suspend operator fun invoke(gift: Gift, friendId: String): Result<Unit> {
+        val beforeGift = giftRepository.getGift(gift.id).getOrThrow()
+        val isDifferentGiftType = beforeGift.type != gift.type
+
+        return giftRepository.updateGift(gift)
+            .onSuccess {
+                // 친구 정보 업데이트
+                if (isDifferentGiftType) {
+                    val friend = friendRepository.getFriend(friendId).getOrThrow()
+
+                    friendRepository.patchFriend(
+                        friend.copy(
+                            received = if (gift.type == GiftType.RECEIVED) friend.received + 1 else friend.received - 1,
+                            sent = if (gift.type == GiftType.SENT) friend.sent + 1 else friend.sent - 1,
+                        )
+                    )
+                        .onFailure {
+                            Log.d("UpdateGiftUseCase", "친구 정보 업데이트 실패")
+                            Result.failure<Unit>(it)
+                        }
+                }
+            }
+    }
+}
