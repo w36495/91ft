a7b3d33535f2a7ca4bb754cc162d901f5c1cebed
w36495 <w36495@naver.com>
2025-05-09 15:50:59 +0900

    feat : 하단 편집 툴 버튼 추가

--- app/src/main/java/com/w36495/senty/view/component/image/editor/ImageEditorScreen.kt
+++ app/src/main/java/com/w36495/senty/view/component/image/editor/ImageEditorScreen.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.HorizontalDivider
@@ -23,20 +27,29 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
 import coil3.size.Scale
+import com.vsnappy1.extension.noRippleClickable
 import com.w36495.senty.R
 import com.w36495.senty.util.getScreenWidthPx
 import com.w36495.senty.view.component.SentyCenterAlignedTopAppBar
+import com.w36495.senty.view.component.image.editor.model.ImageEditToolType
 import com.w36495.senty.view.screen.ui.theme.SentyTheme
+import com.w36495.senty.view.ui.theme.SentyBlack
 import com.w36495.senty.view.ui.theme.SentyGray20
+import com.w36495.senty.view.ui.theme.SentyWhite
 import com.w36495.senty.view.ui.theme.SentyYellow60
 
 @Composable
@@ -79,19 +92,28 @@ private fun ImageEditorScreen(
                 .background(Color(0xFFFBFBFB)),
         ) {
             Column {
-                Image(
-                    painter = rememberAsyncImagePainter(
-                        model = ImageRequest.Builder(context)
-                            .data(imageUris[currentPreviewIndex])
-                            .size(screenWidth)
-                            .scale(Scale.FILL)
-                            .build()
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .weight(1f)
-                        .fillMaxWidth(),
-                )
+                Box(
+                    modifier = Modifier.weight(1f)
+                        .fillMaxWidth()
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            model = ImageRequest.Builder(context)
+                                .data(imageUris[currentPreviewIndex])
+                                .size(screenWidth)
+                                .scale(Scale.FILL)
+                                .build()
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.Center),
+                    )
+
+                    EditToolBox(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 8.dp),
+                    )
+                }
 
                 BottomSmallPreviewSection(
                     imageUris = imageUris,
@@ -121,6 +143,65 @@ private fun ImageEditorHeader(
                     .padding(14.dp),
             )
         }
+    )
+}
+
+@Composable
+private fun EditToolBox(
+    modifier: Modifier = Modifier,
+    onClickTool: (ImageEditToolType) -> Unit = {},
+) {
+    Row(
+        modifier = modifier
+            .background(SentyBlack.copy(0.6f), RoundedCornerShape(14.dp))
+            .padding(vertical = 8.dp, horizontal = 36.dp),
+        horizontalArrangement = Arrangement.spacedBy(24.dp),
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_round_crop_24),
+            contentDescription = "image editor crop icon",
+            tint = SentyWhite,
+            modifier = Modifier
+                .noRippleClickable { onClickTool(ImageEditToolType.CROP) },
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_round_flip_24),
+            contentDescription = "image editor crop icon",
+            tint = SentyWhite,
+            modifier = Modifier
+                .noRippleClickable { onClickTool(ImageEditToolType.FLIP_HORIZONTAL) },
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_round_flip_24),
+            contentDescription = "image editor crop icon",
+            tint = SentyWhite,
+            modifier = Modifier
+                .rotate(90f)
+                .noRippleClickable { onClickTool(ImageEditToolType.FLIP_VERTICAL) },
+        )
+    }
+}
+
+@Composable
+fun CropBoxOverlay(
+    cropRect: Rect,
+    onCropRectChanged: (Rect) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .offset { IntOffset(cropRect.left.toInt(), cropRect.top.toInt()) }
+            .size(cropRect.width.dp) // 정사각형이므로 width = height
+            .border(2.dp, Color.White)
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    val newLeft = cropRect.left + dragAmount.x
+                    val newTop = cropRect.top + dragAmount.y
+                    onCropRectChanged(Rect(newLeft, newTop, newLeft + cropRect.width, newTop + cropRect.width))
+                }
+            }
     )
 }
 
---
+++ app/src/main/java/com/w36495/senty/view/component/image/editor/model/ImageEditToolType.kt
@@ -0,0 +1,7 @@
+package com.w36495.senty.view.component.image.editor.model
+
+enum class ImageEditToolType {
+    CROP,
+    FLIP_HORIZONTAL,
+    FLIP_VERTICAL,
+}
